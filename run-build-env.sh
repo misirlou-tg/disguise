4382d2d6cbe152edd0a161823e5a5df8d61435bf
misirlou-tg <39652424+misirlou-tg@users.noreply.github.com>
2024-02-26 06:01:26 -0600

    Allowing DISGUISE_VOL to override volume name
    Also updating comments for building the application to build the production configuration

--- run-build-env.sh
+++ run-build-env.sh
@@ -1,8 +1,11 @@
+# Usage: run-build-env.sh [volume-name]
+#   if volume-name is omitted $DISGUISE_VOL or 'disguise-master' is used
+#
 # Run a container where Disguise can be built
 # - Mounts the git workspace (as read-only) in /home/node/disguise-ro
 # - Also mounts/creates a docker volume in /home/node/artifacts
 #
-# Typical commands to build & copy out the artifacts:
+# Typical commands to build & copy the artifacts to the volume:
 # cd /home/node
 # mkdir disguise
 # cp -r disguise-ro/* disguise
@@ -10,13 +13,15 @@
 # cd disguise
 # npm install -g @angular/cli@9.0.7
 # npm install
-# ng build
+# ng build -c=production
 # cp -r docs/* ../artifacts
 
 # Get the absolute path to where the script is (--mount needs the full path)
 SCRIPT=$(readlink -f "$0")
 DIR=$(dirname "$SCRIPT")
+DISGUISE_VOL=${DISGUISE_VOL:-${1:-disguise-master}}
+echo Volume $DISGUISE_VOL will be mounted in /home/node/artifacts
 docker run -it --rm --entrypoint /bin/bash \
   --mount type=bind,src=$DIR,dst=/home/node/disguise-ro,readonly \
-  --mount src=disguise-master,dst=/home/node/artifacts \
+  --mount src=$DISGUISE_VOL,dst=/home/node/artifacts \
   node:12.22.12-buster-slim
